21427a50dcde025f7f64bbba6927448941adde0b
georgejrdev <georgejrdev@gmail.com>
2025-01-23 15:03:33 -0300

    Created User

---
+++ backend/src/main/java/com/georgejrdev/study_planner/common/utils/PasswordUtils.java
@@ -0,0 +1,16 @@
+package com.georgejrdev.study_planner.common.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public static String encodePassword(String rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    public static boolean matches(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
